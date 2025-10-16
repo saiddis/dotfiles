6ab73242daab65460d482b9218a4a3bf46ae4da4
saiddis <saiddisruptive@gmail.com>
2025-10-16 14:22:58 +0500

    refactor: remove harpoon & add custom marks keymaps

--- nvim/.config/nvim/lua/saiddis/plugins/marks.lua
+++ nvim/.config/nvim/lua/saiddis/plugins/marks.lua
@@ -1,5 +1,20 @@
 return {
-	"chentoast/marks.nvim",
-	event = "VeryLazy",
-	opts = {},
+  'chentoast/marks.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  config = function()
+    require('marks').setup {
+      default_mappings = false,
+      mappings = {
+        set_next = 'm,',
+        next = '<C-j>',
+        prev = '<C-k>',
+        set_bookmark1 = 'm1',
+        next_bookmark1 = '<C-n>',
+        preview = 'm:',
+        prev_bookmark1 = '<C-p>',
+        delete_buf = 'dm<leader>',
+      },
+    }
+  end,
 }
