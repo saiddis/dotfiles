c85c328149f67b8f887b6e892f55b76e8ea23bdf
saiddis <saiddisruptive@gmail.com>
2025-11-17 10:10:39 +0500

    fix: wsl compatible copy/paste

--- nvim/.config/nvim/lua/saiddis/options.lua
+++ nvim/.config/nvim/lua/saiddis/options.lua
@@ -125,3 +125,12 @@ vim.lsp.enable {
 }
 
 vim.opt.laststatus = 0 -- Or 3 for global statusline
+
+if vim.fn.has('wsl') == 1 then
+    vim.api.nvim_create_autocmd('TextYankPost', {
+        group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+        callback = function()
+            vim.fn.system('clip.exe', vim.fn.getreg('"'))
+        end,
+    })
+end
