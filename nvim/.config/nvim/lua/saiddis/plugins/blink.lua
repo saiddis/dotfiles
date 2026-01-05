b593b82d888b7eb8d2e77c69af0d7c28956c58fd
saiddis <saiddisruptive@gmail.com>
2026-01-05 09:43:34 +0500

    fix: completion preview

--- nvim/.config/nvim/lua/saiddis/plugins/blink.lua
+++ nvim/.config/nvim/lua/saiddis/plugins/blink.lua
@@ -1,6 +1,8 @@
 return {
   'saghen/blink.cmp',
   dependencies = {
+    'rafamadriz/friendly-snippets',
+    'brenoprata10/nvim-highlight-colors',
     {
       'supermaven-inc/supermaven-nvim',
       opts = {
@@ -13,6 +15,48 @@ return {
     },
   },
   opts = {
+    appearance = {
+
+      nerd_font_variant = 'mono',
+    },
+    completion = {
+      documentation = { auto_show = true },
+
+      menu = {
+        draw = {
+          components = {
+            -- customize the drawing of kind icons
+            kind_icon = {
+              text = function(ctx)
+                -- default kind icon
+                local icon = ctx.kind_icon
+                -- if LSP source, check for color derived from documentation
+                if ctx.item.source_name == 'LSP' then
+                  local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                  if color_item and color_item.abbr ~= '' then
+                    icon = color_item.abbr
+                  end
+                end
+                return icon .. ctx.icon_gap
+              end,
+              highlight = function(ctx)
+                -- default highlight group
+                local highlight = 'BlinkCmpKind' .. ctx.kind
+                -- if LSP source, check for color derived from documentation
+                if ctx.item.source_name == 'LSP' then
+                  local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                  if color_item and color_item.abbr_hl_group then
+                    highlight = color_item.abbr_hl_group
+                  end
+                end
+                return highlight
+              end,
+            },
+          },
+        },
+      },
+    },
+
     sources = {
       default = { 'lsp', 'path', 'supermaven', 'snippets', 'buffer' },
       providers = {
